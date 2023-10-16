1fa7e891b09cc4c10cdd1262afed5e13b4e8932e
Amisha <shahiamisha809@gmail.com>
2023-10-16 22:04:40 +0530

    Your commit message here

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,17 +7,30 @@ import Footer from './Components/footer/Footer';
 import FavouriteDabba from './pages/FavouriteDabba/FavouriteDabba';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
 import Login from "./pages/login/Login"
+import FetchProducts from "./Components/searchelements/FetchProducts"
+import Error from "./Components/searchelements/Errors"
+import SingleProduct from "./Components/searchelements/SingleProduct"
+import ScrollToTop from './Components/searchelements/ScrollToTop';
+import PaymentForm from './Components/searchelements/PaymentForm'
 
 function App() {
   return (
       <>
         <BrowserRouter>
+        <ScrollToTop />
+
           <Navbar />
           <Routes>
             <Route path="/" element={<FrontPage />} />
             <Route path="/contactUs" element={<ContactUs />} />
             <Route exact path="/ProfilePage" element={<ProfilePage />} />
             <Route exact path="/FavouriteDabba" element={<FavouriteDabba />} />
+            
+            <Route path="/fetch-products" element={<FetchProducts />}></Route>
+        <Route path="/:name" element={<SingleProduct />}></Route>
+        <Route path="/error" element={<Error />}></Route>
+        <Route path="/payment-form" element={<PaymentForm />} />
+
             <Route path="/login" element={<Login />} />
 
           </Routes>
