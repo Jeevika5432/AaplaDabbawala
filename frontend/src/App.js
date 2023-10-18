c70f8fdbf024693d9a7efca839b65cb5450ec812
Sarah Khan <sarahejaz77@gmail.com>
2023-10-18 18:53:16 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,86 @@
-import './App.css';
-import FrontPage from './pages/FrontPage/FrontPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/navbar/Navbar';
-import ContactUs from './Components/contact/ContactUs';
-import Footer from './Components/footer/Footer';
-import FavouriteDabba from './pages/FavouriteDabba/FavouriteDabba';
-import ProfilePage from './pages/ProfilePage/ProfilePage';
-import Login from "./pages/login/Login"
-import FetchProducts from "./Components/searchelements/FetchProducts"
-import Error from "./Components/searchelements/Errors"
-import SingleProduct from "./Components/searchelements/SingleProduct"
-import ScrollToTop from './Components/searchelements/ScrollToTop';
-import PaymentForm from './Components/searchelements/PaymentForm';
-import Bookings from './Components/Mybookings/Bookings';
+import "./App.css";
+import FrontPage from "./pages/FrontPage/FrontPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/navbar/Navbar";
+import ContactUs from "./Components/contact/ContactUs";
+import Footer from "./Components/footer/Footer";
+import FavouriteDabba from "./pages/FavouriteDabba/FavouriteDabba";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import Login from "../src/pages/login/Login";
+import FormsPage from "./pages/dabbawala/FormsPage";
+import DashboardPage from "./pages/dabbawala/DashboardPage";
+import ServicePage from "./pages/services/ServicePage";
+import FetchProducts from "./Components/searchelements/FetchProducts";
+import Error from "./Components/searchelements/Errors";
+import SingleProduct from "./Components/searchelements/SingleProduct";
+import ScrollToTop from "./Components/searchelements/ScrollToTop";
+import PaymentForm from "./Components/searchelements/PaymentForm";
+import Logindabba from "./pages/Logindabba/Logindabba.js";
+import Bookings from "./Components/Mybookings/Bookings";
+import { UserProvider } from "./context/UserContext";
+import { DabbaProvider } from "./context/DabbaContext";
+
 function App() {
-  return (
-      <>
-        <BrowserRouter>
-        <ScrollToTop />
+    return (
+        <>
+            <UserProvider>
+                <DabbaProvider>
+                    <BrowserRouter>
+                        <ScrollToTop />
 
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<FrontPage />} />
-            <Route path="/contactUs" element={<ContactUs />} />
-            <Route exact path="/ProfilePage" element={<ProfilePage />} />
-            <Route exact path="/FavouriteDabba" element={<FavouriteDabba />} />
-            <Route exact path="/MyBookings" element={<Bookings/>}/> 
-            <Route  exact path="/MyBookings" element={<Bookings/>} />
-
-            <Route path="/fetch-products" element={<FetchProducts />}></Route>
-        <Route path="/:name" element={<SingleProduct />}></Route>
-        <Route path="/error" element={<Error />}></Route>
-        <Route path="/payment-form" element={<PaymentForm />} />
-
-            <Route path="/login" element={<Login />} />
-
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      </>
-  );
+                        <Navbar />
+                        <Routes>
+                            <Route path="/" element={<FrontPage />} />
+                            <Route path="/contactUs" element={<ContactUs />} />
+                            <Route
+                                exact
+                                path="/ProfilePage"
+                                element={<ProfilePage />}
+                            />
+                            <Route
+                                exact
+                                path="/FavouriteDabba"
+                                element={<FavouriteDabba />}
+                            />
+                            <Route
+                                exact
+                                path="/MyBookings"
+                                element={<Bookings />}
+                            />
+                            <Route
+                                path="/fetch-products"
+                                element={<FetchProducts />}
+                            ></Route>
+                            <Route
+                                path="/:name"
+                                element={<SingleProduct />}
+                            ></Route>
+                            <Route path="/error" element={<Error />}></Route>
+                            <Route
+                                path="/payment-form"
+                                element={<PaymentForm />}
+                            />
+                            <Route path="/login" element={<Login />} />
+                            <Route
+                                path="/dabbawalaform"
+                                element={<FormsPage />}
+                            />
+                            <Route path="/service" element={<ServicePage />} />
+                            <Route
+                                path="/dashboard"
+                                element={<DashboardPage />}
+                            />
+                            <Route
+                                path="/logindabba"
+                                element={<Logindabba />}
+                            />
+                        </Routes>
+                        <Footer />
+                    </BrowserRouter>
+                </DabbaProvider>
+            </UserProvider>
+        </>
+    );
 }
 
 export default App;
